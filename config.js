c9d7f86c50fed90720c0a58e64747f9fba6b72c1
rad86679-gif <rad86679@gmail.com>
2026-06-05 21:10:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BOT_TOKEN: "ambil token di bot father",
+  OWNER_IDS: ["di rose"]
+};
+
+
+// Base ini di rakit atau di ciptakan oleh @RZOKISAP jangan merusak username dan jangan sebar base!! ada lapor 
